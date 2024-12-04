665861bdf63fbb79a4319acc8203ededc3cd177c
Fiqih <5026231096@student.its.ac.id>
2024-12-04 17:38:11 +0700

    initial commit

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #12
+ * 1 - 5026231096 - Muhammad Fiqih Soetam Putra
+ * 2 - 5026231164 - Bagus Subekti
+ */
+
 import java.awt.*;
 /**
  * The Cell class models each individual cell of the game board.
